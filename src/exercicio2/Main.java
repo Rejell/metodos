a2525105f9e6cd1530d3c8f482216fbcb38ec4f2
Rejell <rejell@gmail.com>
2022-10-26 10:19:39 -0300

    Exercicio de sobrecarga

---
+++ src/exercicio2/Main.java
@@ -0,0 +1,16 @@
+package exercicio2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Sobrecarga
+        System.out.println("Exercício de cálculo de área");
+        Sobrecarga.area(3);
+        Sobrecarga.area(5d, 5d);
+        Sobrecarga.area(7, 8, 9);
+        Sobrecarga.area(5f,5f);
+
+    }
+
+}
---
+++ src/exercicio2/Sobrecarga.java
@@ -0,0 +1,29 @@
+package exercicio2;
+
+public class Sobrecarga {
+
+    public static void area(double lado) {
+
+        System.out.println(" Área do quadrado: " + lado * lado);
+
+    }
+
+    public static void area(double lado1, double lado2) {
+
+        System.out.println(" Área do retângulo: " + lado1 * lado2);
+
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println(" Área do trapésio: " + ((baseMaior + baseMenor) * altura) / 2);
+
+    }
+
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println(" Área do losango: " + ((diagonal1 * diagonal2) / 2));
+
+    }
+
+}
